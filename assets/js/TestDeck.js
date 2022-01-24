774fd937e545bcea4a23274b177ec7faf94dc7e5
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-01-24 15:27:49 -0500

    Styling for home done. Added test decks and dynamically populate list

---
+++ assets/js/TestDeck.js
@@ -0,0 +1,44 @@
+const saladDeck = {
+    deckName: "Salads",
+    cards: [
+        {
+            name: "Ginger Salad (House Salad)",
+            items: [
+                "Organic Greens",
+                "Tomatoes",
+                "Carrots",
+                "Cucumbers",
+                "Ginger Dressing"
+            ],
+            dietary: [
+                "Vegitarian",
+                "Vegan"
+            ]
+        },
+        {
+            name: "Wakame Salad",
+            items: [
+                "Jelly Seaweed",
+                "Sesame Oil Vinaigrette"
+            ],
+            dietary: [
+                "Vegitarian",
+                "Vegan",
+            ]
+        },
+        {
+            name: "Warm Spinach & Shiitake Miso Salad",
+            items: [
+                "Sauteed shiitake mushroom",
+                "Warm miso vinaigrette",
+                "Fresh spinach",
+                "Toasted macadamia nuts"
+            ],
+            dietary: [
+                "Vegitarian",
+                "*Vegan",
+                "Must ask chef to prepare with oil, not butter"
+            ]
+        }
+    ]
+};
